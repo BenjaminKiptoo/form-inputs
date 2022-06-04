98432a1a7c4c6f023dd699cf67c74193492810ac
Benjamin <kiptoo925@gmail.com>
2022-06-04 11:17:59 +0300

    form-input webpage with select-boxes,radio-buttons,date and color

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const beverage = $("#beverage").val();
+const cars = parseInt($("cars").val());
+const flavor = $("input:radio[name=flavor]:checked").val();
+const dob = $("#born").val();
+const favoriteColor = $("#color").val();
